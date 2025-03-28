d373799ed104ce1ef9d0c3b2f5d292a0c6950d30
corentin <corentin.second@ynov.com>
2025-03-28 10:34:47 +0100

    add SetTimer and Timer

---
+++ main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+// Struct Categories
+type Categories struct {
+	Album        string
+	Artist       string
+	MusicGroup   string
+	Song         string
+	MusicalGenre string
+}
+
+// Struct Player
+type Player struct {
+	Name      string
+	Score     int
+	Responses []string
+}
+
+// Struct of the Game
+type Game struct {
+	RandomLetter        string   // Random letter choose by the computer
+	Round               int      // Round of the game
+	LetterAlreadyChoose []string // Letter already choose by the computer
+	Timer               int      // Timer of the game
+	Players             []Player // Players of the game
+}
+
+// Function main
+func main() {
+	game := Game{}
+
+	game.SetTimer()
+
+	// Random letter
+	letter := game.RandomLetterFunc()
+	println(letter)
+
+
+	Time := make(chan bool)
+	go func() {
+		time.Sleep(time.Duration(game.Timer) * time.Second)
+		Time <- true
+	}()
+
+	go func() {
+		for {
+			// Check first letter
+			result := game.CheckFirstLetter(game.UserInput())
+			println(result)
+			Time <- true
+		}
+	}()
+
+	<-Time
+	println("Finish")
+	
+}
+
+func (g *Game) SetTimer(){
+	fmt.Println("how long did the tour last (in secondes)?")
+	_, _ = fmt.Scanln(&g.Timer)
+	if g.Timer <= 0 {
+		fmt.Println("The time must be over 0 seconds, otherwise the game will not start.")
+		g.SetTimer()
+	}
+	if g.Timer > 0 {
+		g.Timer = g.Timer 
+	}
+}
+
+// User Input (temporary)
+func (g *Game) UserInput() string {
+	var WordInput string
+
+	println("Enter a word")
+	_, _ = fmt.Scanln(&WordInput)
+
+	return WordInput
+}
+
+// Random letter
+func (g *Game) RandomLetterFunc() string {
+	rand.NewSource(time.Now().UnixNano())
+
+	for {
+		g.RandomLetter = string(rune(rand.Intn(26) + 65))
+
+		alreadyChoosen := false
+		for _, letter := range g.LetterAlreadyChoose {
+			if letter == g.RandomLetter {
+				alreadyChoosen = true
+				break
+			}
+		}
+
+		if !alreadyChoosen {
+			break
+		}
+
+		if len(g.LetterAlreadyChoose) >= 26 {
+			return ""
+		}
+	}
+
+	g.LetterAlreadyChoose = append(g.LetterAlreadyChoose, g.RandomLetter)
+	return g.RandomLetter
+}
+
+// Check first letter
+func (g *Game) CheckFirstLetter(word string) bool {
+	if strings.ToUpper(string(rune(word[0]))) == strings.ToUpper(g.RandomLetter) {
+		return true
+	}
+	return false
+}
+
+
