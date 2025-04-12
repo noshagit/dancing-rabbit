02e8c86633de8e05a86191a96f6c83a5aa80fbd3
vnathael <nathael.vigne@ynov.com>
2025-04-12 12:34:57 +0200

    blind test + basic connexion to front

--- backend/games/blind-test.go
+++ backend/games/blind-test.go
@@ -1,30 +1,63 @@
 package main
 
-//package games
-
 import (
+	"encoding/json"
 	"fmt"
 	"log"
-	"math/rand/v2"
+	"math/rand"
 	"net/http"
 	"slices"
+	"strings"
+	"sync"
 	"text/template"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/tsirysndr/go-deezer"
 )
 
 var (
-	playlist      *deezer.Tracks
-	previousSongs []string
-	currentSong   deezer.Track
-	playerGuesses map[string]string
+	client         = deezer.NewClient()
+	shuffledTracks []deezer.Track
+	previousSongs  []string
+	currentSong    deezer.Track
+	playerGuesses  = make(map[string]string)
+	gameState      = GameState{}
+	mutex          = &sync.Mutex{}
+	totalRounds    = 10
+	currentRound   = 0
+	gameStarted    = false
+	roundEndTime   time.Time
+	roundStartTime time.Time
 )
 
+type GameState struct {
+	CurrentRound  int               `json:"currentRound"`
+	TotalRounds   int               `json:"totalRounds"`
+	TimeLeft      int               `json:"timeLeft"`
+	CurrentSong   string            `json:"currentSong"`
+	Players       []Player          `json:"players"`
+	PlayerScores  map[string]int    `json:"playerScores"`
+	PlayerAnswers map[string]string `json:"playerAnswers"`
+	GameStarted   bool              `json:"gameStarted"`
+	RoundActive   bool              `json:"roundActive"`
+	PreviewURL    string            `json:"previewUrl"`
+}
+
+type Player struct {
+	Name   string `json:"name"`
+	Status string `json:"status"`
+	Score  int    `json:"score"`
+}
+
 func main() {
-	getTrack()
 	r := mux.NewRouter()
+
 	r.HandleFunc("/", handler).Methods("GET")
+	r.HandleFunc("/start", startGame).Methods("POST")
+	r.HandleFunc("/guess", handleGuess).Methods("POST")
+	r.HandleFunc("/state", getGameState).Methods("GET")
+	r.HandleFunc("/next-round", nextRound).Methods("POST")
 
 	r.HandleFunc("/blind-test.css", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/css")
@@ -35,36 +68,246 @@ func main() {
 		http.ServeFile(w, r, "../../frontend/blind-test/game/blind-test.js")
 	})
 
+	initGameState()
+
 	fmt.Println("Server started on :8080")
 	http.ListenAndServe(":8080", r)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("HANDLER")
+func initGameState() {
+	gameState = GameState{
+		CurrentRound:  0,
+		TotalRounds:   totalRounds,
+		TimeLeft:      30,
+		CurrentSong:   "",
+		Players:       []Player{},
+		PlayerScores:  make(map[string]int),
+		PlayerAnswers: make(map[string]string),
+		GameStarted:   false,
+		RoundActive:   false,
+		PreviewURL:    "",
+	}
+}
 
+func handler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("../../frontend/blind-test/game/blind-test.html"))
-	err2 := tmpl.Execute(w, currentSong.Preview)
-	if err2 != nil {
-		log.Fatalf("Failed to execute template: %v", err2)
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		log.Fatalf("Failed to execute template: %v", err)
+	}
+}
+
+func startGame(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	var players []string
+	err := json.NewDecoder(r.Body).Decode(&players)
+	if err != nil {
+		http.Error(w, "Invalid player data", http.StatusBadRequest)
+		return
+	}
+
+	gameState.Players = make([]Player, len(players))
+	for i, name := range players {
+		gameState.Players[i] = Player{
+			Name:   name,
+			Status: "waiting",
+			Score:  0,
+		}
+		gameState.PlayerScores[name] = 0
+	}
+
+	gameStarted = true
+	currentRound = 0
+	previousSongs = []string{}
+	shuffledTracks = loadAndShufflePlaylist()
+	gameState.GameStarted = true
+
+	startRound()
+
+	json.NewEncoder(w).Encode(gameState)
+}
+
+func loadAndShufflePlaylist() []deezer.Track {
+	playlist, err := client.Playlist.GetTracks("12739384001")
+	if err != nil || len(playlist.Data) == 0 {
+		log.Fatalf("Failed to load playlist: %v", err)
+	}
+
+	tracks := playlist.Data
+	rand.Shuffle(len(tracks), func(i, j int) {
+		tracks[i], tracks[j] = tracks[j], tracks[i]
+	})
+	return tracks
+}
+
+func startRound() {
+	if currentRound >= totalRounds {
+		endGame()
+		return
+	}
+
+	currentRound++
+	gameState.CurrentRound = currentRound
+	getTrack()
+
+	gameState.CurrentSong = currentSong.Title
+	gameState.PreviewURL = currentSong.Preview
+	gameState.RoundActive = true
+	gameState.TimeLeft = 30
+	roundStartTime = time.Now()
+	roundEndTime = roundStartTime.Add(30 * time.Second)
+
+	for name := range gameState.PlayerScores {
+		gameState.PlayerAnswers[name] = ""
+	}
+
+	for i := range gameState.Players {
+		gameState.Players[i].Status = "playing"
+	}
+
+	go func() {
+		time.Sleep(30 * time.Second)
+		endRound()
+	}()
+}
+
+func endRound() {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	gameState.RoundActive = false
+
+	for i, player := range gameState.Players {
+		if gameState.PlayerAnswers[player.Name] == "" {
+			gameState.Players[i].Status = "timeout"
+		}
+	}
+
+	go func() {
+		time.Sleep(10 * time.Second)
+		nextRound(nil, nil)
+	}()
+}
+
+func endGame() {
+	gameState.GameStarted = false
+	gameStarted = false
+	gameState.RoundActive = false
+	gameState.CurrentRound = totalRounds
+}
+
+func nextRound(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	startRound()
+
+	if w != nil {
+		json.NewEncoder(w).Encode(gameState)
 	}
 }
 
 func getTrack() {
-	fmt.Println("GET TRACK")
-
-	client := deezer.NewClient()
-	playlist, err1 := client.Playlist.GetTracks("53362031")
-	if err1 != nil {
-		log.Fatalf("Failed to get playlist: %v", err1)
-	}
-
-	randomTrackIndex := rand.IntN(playlist.Total)
-	randomTrack := playlist.Data[randomTrackIndex]
-	if slices.Contains(previousSongs, randomTrack.Title) {
-		fmt.Println("song already played")
-		getTrack()
+	if len(shuffledTracks) == 0 {
+		log.Println("No more songs left in shuffled list.")
+		currentSong = deezer.Track{
+			Title:   "No Song",
+			Preview: "",
+		}
 		return
 	}
-	currentSong = randomTrack
-	previousSongs = append(previousSongs, randomTrack.Title)
+
+	for len(shuffledTracks) > 0 {
+		track := shuffledTracks[0]
+		shuffledTracks = shuffledTracks[1:]
+
+		if !slices.Contains(previousSongs, track.Title) {
+			currentSong = track
+			previousSongs = append(previousSongs, track.Title)
+			log.Printf("Selected track: %+v", currentSong)
+			return
+		}
+	}
+
+	log.Println("Ran out of unique songs, reloading playlist.")
+	shuffledTracks = loadAndShufflePlaylist()
+	getTrack()
+}
+
+func handleGuess(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	if !gameState.RoundActive {
+		http.Error(w, "Round is not active", http.StatusBadRequest)
+		return
+	}
+
+	type Guess struct {
+		Player string `json:"player"`
+		Answer string `json:"answer"`
+	}
+
+	var guess Guess
+	err := json.NewDecoder(r.Body).Decode(&guess)
+	if err != nil {
+		http.Error(w, "Invalid guess data", http.StatusBadRequest)
+		return
+	}
+
+	found := false
+	for i, player := range gameState.Players {
+		if player.Name == guess.Player {
+			found = true
+			if gameState.PlayerAnswers[player.Name] == "" {
+				gameState.PlayerAnswers[player.Name] = guess.Answer
+
+				if strings.ToLower(guess.Answer) == strings.ToLower(currentSong.Title) {
+					timeElapsed := time.Since(roundStartTime)
+					score := calculateScore(timeElapsed)
+					gameState.PlayerScores[player.Name] += score
+					gameState.Players[i].Score = gameState.PlayerScores[player.Name]
+					gameState.Players[i].Status = "correct"
+				} else {
+					gameState.Players[i].Status = "wrong"
+				}
+			}
+			break
+		}
+	}
+
+	if !found {
+		http.Error(w, "Player not found", http.StatusNotFound)
+		return
+	}
+
+	json.NewEncoder(w).Encode(gameState)
+}
+
+func calculateScore(timeElapsed time.Duration) int {
+	secondsElapsed := int(timeElapsed.Seconds())
+	if secondsElapsed > 30 {
+		secondsElapsed = 30
+	}
+	score := 100 - (secondsElapsed * 3)
+	if score < 10 {
+		score = 10
+	}
+	return score
+}
+
+func getGameState(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	if gameState.RoundActive {
+		gameState.TimeLeft = int(roundEndTime.Sub(time.Now()).Seconds())
+		if gameState.TimeLeft < 0 {
+			gameState.TimeLeft = 0
+		}
+	}
+
+	json.NewEncoder(w).Encode(gameState)
 }
